13d5d13c4199004cc954fe5edd7130032ff634cf
vinyl810 <19thsummer@kaist.ac.kr>
2023-04-10 01:14:46 +0900

    fix style for mobile

--- src/components/bird/BirdComponent.tsx
+++ src/components/bird/BirdComponent.tsx
@@ -24,7 +24,7 @@ function Bird() {
   return (
     <div
       className="ml-5 self-stretch rounded-md bg-slate-100 shadow-lg"
-      style={{ width: '90vw', height: '80vh' }}
+      style={{ width: '90vw', height: '70vh' }}
     >
       <Canvas shadows camera={{ position: [-6, 3, 6] }} frameloop="demand">
         <ambientLight intensity={1} />
--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <div className="mb-5 flex h-12 w-screen items-center justify-center bg-zinc-500 pb-3 pt-3 font-thin text-stone-600 drop-shadow-md">
+    <div className="mb-5 flex h-12 w-full items-center justify-center bg-zinc-500 pb-3 pt-3 font-thin text-stone-600 drop-shadow-md">
       <div className="btn rounded-md bg-slate-100 pl-4 pr-4 text-lg shadow-md shadow-zinc-400">
         {'<TextBird />'}
       </div>
